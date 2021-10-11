f952728c4c7610f9ff16dfc02274129a7de2bbd9
sjuenger <steffen.juenger@gmx.de>
2021-10-11 17:19:37 +0200

    * Usage of Sparqljs to convert the found SPARQL Queries (those with references) to JSON * hand over the found queries to sparqljs (executed with node.js) via a tmp.sparql file * Write the results to a specific file

--- SPARQLParserTest.py
+++ SPARQLParserTest.py
@@ -1,22 +1,42 @@
+import subprocess
 import gzip
+import json
 import csv
-from urllib.parse import unquote
+from urllib.parse import unquote_plus
+
 #import fyzz
 
 with gzip.open('data/2017-06-12_2017-07-09_organic.tsv.gz', 'rt') as sample_data:
-    read_tsv = csv.reader(sample_data, delimiter="\t")
-    i=0
-    sample=""
-    check_file=[]
-    for row in read_tsv:
-        sample = unquote(row[0])
-        if("<http://www.wikidata.org/prop/reference" in sample):
-            sample = sample.replace("+", " ")
-            print(sample)
-            if(sample in check_file) :
-                print(sample)
-                i+=1
-                check_file.append((sample, 1)) #add sample | +1 in  the detection
-                print("References found: ", i, "\n")
+    with gzip.open('data/2017-06-12_2017-07-09_organic.json.gz','wt') as result_data:
+        with open('data/tmp.sparql','wt') as hand_over_to_js_data:
+            read_tsv = csv.reader(sample_data, delimiter="\t")
+            i=0
+            sample=""
+            check_file=[]
+            for row in read_tsv:
+                sample = unquote_plus(row[0])
+                if("<http://www.wikidata.org/prop/reference" in sample):
+                    i += 1
+                    hand_over_to_js_data.seek(0)
+                    hand_over_to_js_data.write(sample)
+                    hand_over_to_js_data.truncate()
 
-    sample_data.close()
+                    process = subprocess.Popen(["sparqljs", "--strict", "./data/tmp.sparql"], stdout=subprocess.PIPE)
+                    output_bytes, err = process.communicate()
+
+                    output_str = output_bytes.decode("utf-8")
+
+                    output_json = json.loads(output_str)
+
+                    output_json["timestamp"] = row[1]
+                    output_json["sourceCategory"] = row[2]
+                    output_json["user_agent"] = row[3]
+
+                    json.dump(output_json, result_data)
+
+                    if (i == 1):
+                        break
+
+        hand_over_to_js_data.close()
+    result_data.close()
+sample_data.close()
