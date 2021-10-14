1e837d9673f26bc16cb643dc52daae79ee498222
sjuenger <steffen.juenger@gmx.de>
2021-10-14 16:42:25 +0200

    Changed the way, the json files are stored. * Now, there is a folder per data source and each query has its own file

--- extractSPARQLtoJSON.py
+++ extractSPARQLtoJSON.py
@@ -2,39 +2,53 @@ import subprocess
 import gzip
 import json
 import csv
+import time
 from urllib.parse import unquote_plus
 
 
-def extract():
-    with gzip.open('data/2017-06-12_2017-07-09_organic.tsv.gz', 'rt') as sample_data:
-        with gzip.open('data/2017-06-12_2017-07-09_organic.json.gz','wt') as result_data:
-            read_tsv = csv.reader(sample_data, delimiter="\t")
-            i = 0
-            sample = ""
-            check_file = []
-            for row in read_tsv:
-                sample = unquote_plus(row[0])
-                if("<http://www.wikidata.org/prop/reference" in sample):
-                    i += 1
+def extract(location):
 
-                    process = subprocess.Popen(["./modules/sparqljs/bin/sparql-to-json", "--strict", sample], stdout=subprocess.PIPE)
-                    output_bytes, err = process.communicate()
-                    print(err)
+    tsv_data_path = "data/" + location + ".tsv.gz"
 
-                    if (err == None):
-                        output_str = output_bytes.decode("utf-8")
+    with gzip.open(tsv_data_path, 'rt') as sample_data:
 
-                        output_json = json.loads(output_str)
+        read_tsv = csv.reader(sample_data, delimiter="\t")
+        i = 0
+        sample = ""
+        check_file = []
 
-                        output_json["timestamp"] = row[1]
-                        output_json["sourceCategory"] = row[2]
-                        output_json["user_agent"] = row[3]
-                        print(i)
+        start_time = time.time()
 
+        for row in read_tsv:
+            sample = unquote_plus(row[0])
+
+            if("<http://www.wikidata.org/prop/reference" in sample):
+                i += 1
+
+                process = subprocess.Popen(["./modules/sparqljs/bin/sparql-to-json",
+                                            "--strict", sample], stdout=subprocess.PIPE)
+                output_bytes, err = process.communicate()
+                print(err)
+
+                if (err == None):
+                    output_str = output_bytes.decode("utf-8")
+
+                    output_json = json.loads(output_str)
+
+                    output_json["timestamp"] = row[1]
+                    output_json["sourceCategory"] = row[2]
+                    output_json["user_agent"] = row[3]
+                    print("Queries gefunden: " , i)
+                    print("Bisher benötigte Zeit in min: ", (time.time()-start_time)/60 )
+                    print("\n")
+
+                    path_to_json = "data/" + location + "/" + str(i) + ".json"
+
+                    with open(path_to_json, "wt") as result_data:
                         json.dump(output_json, result_data)
+                    result_data.close()
 
-            print("Pffff.... that took a long time.")
-            print(i)
+        print("Pffff.... that took a long time.")
+        print(i)
 
-        result_data.close()
     sample_data.close()
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
 import extractSPARQLtoJSON
 
-extractSPARQLtoJSON.extract()
+location = "2017-06-12_2017-07-09_organic"
+
+extractSPARQLtoJSON.extract(location)
