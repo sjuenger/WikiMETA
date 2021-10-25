3867778278aea8b7e40ab71a68ad182f5a750a29
sjuenger <steffen.juenger@gmx.de>
2021-10-25 12:23:04 +0200

    Added ranks as multidata and added some error protection in case, a conversion goes wrong. Then, the faulty? sparql query is saved in a specific folder

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sparql_to_json_references
+import sparql_to_json_qualifiers
+import sparql_to_json_ranks
 import bound_references
 import redundant_detection
 
@@ -6,6 +8,8 @@ import redundant_detection
 LOCATION = "2017-06-12_2017-07-09_organic"
 # TODO: Wirte a script, which build an directory structure fpr extractSPARQLtoJSON
 
-sparql_to_json_references.extract_SPARQL_to_JSON(LOCATION)
+sparql_to_json_qualifiers.extract_SPARQL_to_JSON(LOCATION)
+#sparql_to_json_references.extract_SPARQL_to_JSON(LOCATION)
+#sparql_to_json_ranks.extract_SPARQL_to_JSON(LOCATION)
 #bound_references.find_bound_references(LOCATION)
 #redundant_detection.delete_redundant_queries(LOCATION)
--- sparql_to_json_ranks.py
+++ sparql_to_json_ranks.py
@@ -5,76 +5,40 @@ import csv
 import time
 from urllib.parse import unquote_plus
 
-
 def extract_SPARQL_to_JSON(location):
     tsv_data_path = "data/" + location + ".tsv.gz"
 
     with gzip.open(tsv_data_path, 'rt') as sample_data:
 
         read_tsv = csv.reader(sample_data, delimiter="\t")
+
         i = 0
         failed_conversions = 0
 
         start_time = time.time()
 
         k = 0
-
         for row in read_tsv:
             sample_sparql = unquote_plus(row[0])
-
             print(k)
             k += 1
-
-            # Information for the strings from https://www.mediawiki.org/wiki/Wikibase/Indexing/RDF_Dump_Format
-
-            contains = "none"
-
-            if "<http://www.w3.org/ns/prov#wasDerivedFrom>" in sample_sparql:
-
-                if "<http://www.wikidata.org/prop/reference" in sample_sparql:
-
-                    if "<http://www.wikidata.org/reference" in sample_sparql:
-                        contains = "all_three"
-                    else:
-                        contains = "derived_+_reference_property"
-
-                elif "<http://www.wikidata.org/reference" in sample_sparql:
-                    contains = "derived_+_reference_node"
-                else:
-                    contains = "only_derived"
-
-            elif "<http://www.wikidata.org/prop/reference" in sample_sparql:
-
-                if "<http://www.wikidata.org/reference" in sample_sparql:
-                    contains = "reference_node_+_reference_property"
-                else:
-                    contains = "only_reference_property"
-
-            elif "<http://www.wikidata.org/reference" in sample_sparql:
-                contains = "only_reference_node"
-
-
-
-
-            if contains != "none":
+            if "<http://wikiba.se/ontology#rank" in sample_sparql:
                 i += 1
 
                 start_time_ref_query = time.time()
-
                 try:
                     process = subprocess.Popen(["./modules/sparqljs/bin/sparql-to-json",
                                                 "--strict", sample_sparql], stdout=subprocess.PIPE)
                     output_bytes, err = process.communicate()
                 except subprocess.CalledProcessError as e:
-                    print(err)
                     print(e)
                     failed_conversions += 1
+                    # // does not go in there !!
 
                 if len(output_bytes) != 0:
-
                     output_str = output_bytes.decode("utf-8")
-                    output_json = json.loads(output_str)
 
+                    output_json = json.loads(output_str)
 
                     output_json["timestamp"] = row[1]
                     output_json["sourceCategory"] = row[2]
@@ -83,9 +47,11 @@ def extract_SPARQL_to_JSON(location):
                     print("Time required so far in min: ", (time.time() - start_time) / 60)
 
                     path_to_json = "data/" + location[:21] + "/" + \
-                                   location[22:] + "/reference_metadata/" + contains + "/" + str(i) + ".json"
+                                   location[22:] + "/rank_metadata/" + "wikibase_rank" + "/" + str(
+                        i) + ".json"
                     path_to_sparql = "data/" + location[:21] + "/" + \
-                                     location[22:] + "/reference_metadata/" + contains + "/" + str(i) + ".sparql"
+                                     location[22:] + "/rank_metadata/" + "wikibase_rank" + "/" + str(
+                        i) + ".sparql"
 
                     with open(path_to_json, "wt") as result_data:
                         json.dump(output_json, result_data)
@@ -104,7 +70,7 @@ def extract_SPARQL_to_JSON(location):
                     print(sample_sparql)
                     failed_conversions += 1
                     path_to_failed = "data/" + location[:21] + "/" + \
-                                  location[22:] + "/reference_metadata/" + "failed_queries" + "/" + str(k)\
+                                  location[22:] + "/rank_metadata/" + "failed_queries" + "/" + str(k)\
                                      + " " + unquote_plus(row[1]) + ".txt"
 
                     with open(path_to_failed, "wt") as failed_txt:
@@ -113,12 +79,13 @@ def extract_SPARQL_to_JSON(location):
                         failed_txt.write(unquote_plus(row[2]))
                         failed_txt.write(unquote_plus(row[3]))
 
+
         path_to_txt = "data/" + location[:21] + "/" + \
-                      location[22:] + "/reference_metadata/" + "property_reference" + "/" + str(
+                      location[22:] + "/rank_metadata/" + "wikibase_rank" + "/" + str(
             failed_conversions) + ".txt"
 
         with open(path_to_txt, "wt") as failed_txt:
-            failed_txt.write(failed_conversions.to_string())
+            failed_txt.write(failed_conversions.toString())
 
         print("Pffff.... that took a long time.")
         print("Total amount of queries found: ", i)
