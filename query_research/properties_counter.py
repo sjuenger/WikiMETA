eb2976eabecc38dcfdd6d307500a048050447d1c
sjuenger <steffen.juenger@gmx.de>
2022-01-21 17:45:48 +0100

    Work in Progress on the property_counter for the query_research.

--- query_research/properties_counter.py
+++ query_research/properties_counter.py
@@ -1,7 +1,21 @@
 # the purpose of this module is, to create a text file, which displays the occurrences of every Wikidata property
 # .. in the available data
-# for this purpose the 'property_dictionary' is used
+# for this purpose the 'property_dictionary.json' is used
 
-
+# mode can either ba "reference" or "qualifier" to describe the usage case as
+# .. a qualifier
+# ->
+# .. a reference
+# ->
 def count_property_in(property, mode):
+
+    if mode not in ["qualifier", "reference"]:
+        error_message = "Not supported mode."
+        raise Exception(error_message)
+
+    # check, if the property dictionary already exists
+    #if
+
+    #with open("")
+
     return
