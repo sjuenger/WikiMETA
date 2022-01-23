eb16fb4e2a7e9c60902c2bc520d51d8cdb4a9917
sjuenger <steffen.juenger@gmx.de>
2022-01-23 18:07:36 +0100

    Next steps on the properties counter. * Idea for a recursive way to search through the dictionary.

--- query_research/properties_counter.py
+++ query_research/properties_counter.py
@@ -7,15 +7,45 @@
 # ->
 # .. a reference
 # ->
-def count_property_in(property, mode):
 
-    if mode not in ["qualifier", "reference"]:
+import glob
+import json
+
+def count_property_in(location, data_type):
+
+    if data_type not in ["qualifier_metadata", "reference_metadata"]:
         error_message = "Not supported mode."
         raise Exception(error_message)
 
-    # check, if the property dictionary already exists
+    # TODO: check, if the property dictionary already exists
     # if
+    # TODO: change to only the files, that do contain a property
+    # Retrieve all files, ending with .json
+    files_json = glob.glob("data/" + location[:21] + "/" +
+                             location[22:] + "/" + data_type + "/*.json")
 
-    #with open("")
+    # get the path to the folder, where the json file about the gathered statistical information
+    # .. is stored
+    path_to_stat_information = "data/" + location[:21] + "/" + location[22:] + "/" + \
+                        data_type.split('/')[0] + "/statistical_information"
 
+    result_dict = {}
+    result_dict["properties"] = {}
+    result_dict["unique_properties"] = 0
+    result_dict["unique_properties"] = 0
+
+    for query_file in files_json:
+        with open(query_file, "r") as query:
+            query_json = json.load(query)
+
+            where_part = query_json["where"]
+
+            search_dict_deep_for_multiple_reference_properties(where_part)
+
+
+    return
+
+def search_dict_deep_for_multiple_reference_properties(current_dict):
+    # recursive fun
+    search_for = "http://www.wikidata.org/prop/reference/P"
     return
