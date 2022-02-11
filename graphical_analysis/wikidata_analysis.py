9ce11ea991d3e6ed415a43b596b0978256904122
sjuenger <steffen.juenger@gmx.de>
2022-02-11 21:27:12 +0100

    Finished the visualisation of the first wikidata statistical information

--- graphical_analysis/wikidata_analysis.py
+++ graphical_analysis/wikidata_analysis.py
@@ -4,6 +4,7 @@ import numpy as nm
 import matplotlib.pyplot as plt
 import glob
 import json
+import collections
 
 def plot_top_wikidata_research_properties():
 
@@ -20,23 +21,45 @@ def plot_top_wikidata_research_properties():
 
             print(files_json)
 
-            # change up the dictionary to match this schema: {"PID": {}, "label":{}, "qualifier": {}}
-
             with open(files_json[0], "r") as json_data:
 
                 whole_dict = json.load(json_data)
 
-                props_dict = whole_dict
+                tmp = collections.OrderedDict(sorted(whole_dict["properties"].items(), key=lambda item: int(item[1][metadata_mode + "_no"]) ))
 
-                print(props_dict["properties"])
+                print(whole_dict)
+                print(tmp)
 
-                df = pd.DataFrame(props_dict["properties"])
+                whole_dict["properties"] = tmp
+
+                # change up the dictionary to match this schema: {"PID": {}, "label":{}, "qualifier": {}}
+                # from this: {'P585': {'label': 'point in time', 'datatype': 'Time', 'statement_no': '833621',
+
+                dataframe_dict = {}
+                dataframe_dict["PID/label"] = []
+                dataframe_dict["qualifier_percentage"] = []
+                dataframe_dict["reference_percentage"] = []
+                dataframe_dict["qualifier_class"] = []
+                dataframe_dict["is_reference"] = []
+
+                for PID in whole_dict["properties"]:
+
+                    dataframe_dict["PID/label"].append(PID + " / " + whole_dict["properties"][PID]["label"])
+                    dataframe_dict["qualifier_percentage"].append( int(whole_dict["properties"][PID]["qualifier_no"]) / int(whole_dict[ "total_usages_of_" + metadata_mode ]) )
+                    dataframe_dict["reference_percentage"].append( int(whole_dict["properties"][PID]["reference_no"]) / int(whole_dict[ "total_usages_of_" + metadata_mode ]) )
+                    dataframe_dict["qualifier_class"].append(whole_dict["properties"][PID]["qualifier_class"])
+                    dataframe_dict["is_reference"].append(whole_dict["properties"][PID]["is_reference"])
+
+                print(dataframe_dict)
+
+                df = pd.DataFrame(dataframe_dict)
 
                 print(df)
 
-                tmp = sns.catplot(x= "P585", y="qualifier_no", data=df)
+                #tmp = sns.catplot(y= "PID/label", x=metadata_mode + "_percentage" , hue="is_reference", kind="bar",  data=df)
+                tmp = sns.catplot(y= "PID/label", x=metadata_mode + "_percentage" , kind="bar",  data=df)
 
-                tmp.savefig("tmp.png")
+                tmp.savefig("tmp_" + metadata_mode + "_" + recommended_mode +".png")
 
 
 
